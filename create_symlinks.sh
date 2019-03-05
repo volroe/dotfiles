2bacbbc3676b9148251596fff067064508785a2a
Volkert Roeloffs <vroeloffs@berkeley.edu>
2019-03-05 12:02:30 -0800

    run create_symlinks.sh to generate symlinks on local machines

---
+++ create_symlinks.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+ln -rsf  /home/vroeloffs/dotfiles/.tmux.conf    /home/vroeloffs/.tmux.conf
+ln -rsf  /home/vroeloffs/dotfiles/.bash_aliases /home/vroeloffs/.bash_aliases
+ln -rsf  /home/vroeloffs/dotfiles/.vimrc        /home/vroeloffs/.vimrc
