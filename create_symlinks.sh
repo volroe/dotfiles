e6612b5dec4b114a7f4efe7ac5329eeb3f60df74
Volkert Roeloffs <39084847+volroe@users.noreply.github.com>
2023-03-09 08:51:42 +0100

    FIX missing dot in tmux conf

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-ln -rsf  $HOME/dotfiles/.tmux.conf    $HOME/tmux.conf
+ln -rsf  $HOME/dotfiles/.tmux.conf    $HOME/.tmux.conf
 ln -rsf  $HOME/dotfiles/.bash_aliases $HOME/.bash_aliases
 ln -rsf  $HOME/dotfiles/.vimrc        $HOME/.vimrc
